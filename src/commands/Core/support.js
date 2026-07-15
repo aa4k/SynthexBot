2c128086ad12c55e06e531c6571f9dde26194a25
aa4k <hussainalsada897@gmail.com>
2026-07-16 01:53:20 +0300

    Update support.js

--- src/commands/Core/support.js
+++ src/commands/Core/support.js
@@ -3,7 +3,7 @@ import { createEmbed } from '../../utils/embeds.js';
 import { logger } from '../../utils/logger.js';
 
 import { InteractionHelper } from '../../utils/interactionHelper.js';
-const SUPPORT_SERVER_URL = "https://discord.gg/QnWNz2dKCE";
+const SUPPORT_SERVER_URL = "https://discord.com/channels/1522665308513636563/1522724147157926048";
 export default {
     data: new SlashCommandBuilder()
     .setName("support")
